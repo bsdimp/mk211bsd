c239f4c0e9ce0e9bfd295e3936d5df550ea907ca
Warner Losh <imp@freebsd.org>
2020-07-15 22:38:20 -0600

    Second stab at the build scripts. It's safer to copy everything only in the chroot, so do it there. Plus try the sequence that reading the Makefiles in /usr/src suggests, avoiding make build becuase (a) it's not quite right for this situation and (b) it fails in the second build of libc for reasons that don't make any sense.
    Add some comments about building the kernel. I need to figure out the best way
    to compare/contrast the kernel Makefile issues still since that's likely the
    largest area of inconsistency remaining.

--- hints/build.sh
+++ hints/build.sh
@@ -7,20 +7,6 @@ R=/scratch
 S=$R/usr/src
 
 #
-# Bootstap as to make .s -> .o. That's done in the recovery script due to the
-# diffuclty of marshalling all the bits to the 2.11 system. Here we just copy
-# the results. We also clean up all the binaries we produced in the hacky way
-# so we don't have issues with permissions and/or timestamps later. We'll
-# also do a make clean, but this is an abundance of caution move at this staage
-# given the turn around time.
-#
-echo Copying bootstrapped as...
-cd $S/bin/as
-cp as $R/bin
-cp as2 $R/lib
-rm -f as as2 *.o
-
-#
 # Once we have the .o files, we need to link them into binaries. We also need to
 # create libraries. ld(1) is what turns .o files into other .o files as well as
 # executables. nm(1) is used by the lorder(1) script to create the long list of
@@ -28,25 +14,12 @@ rm -f as as2 *.o
 # ar(1) here, though, because the ar(5) format is the same, even if the ar(1)
 # program changed to be the one from 4.3BSD in patch 173.
 #
-# Finally, we do technically need ranlib(1) if we don't want to do a crapton of
-# surgery on the source tree, so remove it and copy /bin/true into it's
-# place. This will result in slower link times for the first few binaries we
-# build, but the binaries will be the same and ranlib(1) will be one of those
-# libraries. Except for libc, which has some circular dependencies, so we have
-# to list -lc on the command line below to pull in all those cycles. It won't
-# actually link things multiple times, though, since it only gets .o's that
-# were missed in the first pass.
-#
 echo Bootstrapping ld and nm and faking ranlib
 cd $S/bin
-cc -o ld -O -i -I$S/usr/include ld.c -lc
-cc -o nm -O -i -I$S/usr/include nm.c -lc
-cp nm ld $R/bin
-rm -f ld nm
-rm $R/usr/bin/ranlib
-cp $R/bin/true $R/usr/bin/ranlib
+cc -o ld -O -i -I$R/usr/include ld.c -lc
+cc -o nm -O -i -I$R/usr/include nm.c -lc
 
 # OK, now it's time to jump into the chroot.
 echo Launching the chroot build
 cd /
-chroot $R build2.sh
+chroot $R sh -c /build2.sh
--- hints/build2.sh
+++ hints/build2.sh
@@ -9,12 +9,43 @@
 S=/usr/src
 
 #
+# Bootstap as to make .s -> .o. That's done in the recovery script due to the
+# diffuclty of marshalling all the bits to the 2.11 system. Here we just copy
+# the results. We also clean up all the binaries we produced in the hacky way
+# so we don't have issues with permissions and/or timestamps later. We'll
+# also do a make clean, but this is an abundance of caution move at this staage
+# given the turn around time.
+#
+echo Copying bootstrapped as...
+cd $S/bin/as
+cp as /bin
+cp as2 /lib
+rm -f as as2 *.o
+
+#
+# Copy over the bootstrapped nm and ld.
+#
+# Finally, we do technically need ranlib(1) if we don't want to do a crapton of
+# surgery on the source tree, so remove it and copy /bin/true into it's
+# place. This will result in slower link times for the first few binaries we
+# build, but the binaries will be the same and ranlib(1) will be one of those
+# libraries. Except for libc, which has some circular dependencies, so we have
+# to list -lc on the command line below to pull in all those cycles. It won't
+# actually link things multiple times, though, since it only gets .o's that
+# were missed in the first pass.
+#
+cp nm ld $R/bin
+rm -f ld nm
+rm $R/usr/bin/ranlib
+cp $R/bin/true $R/usr/bin/ranlib
+
+#
 # Time to build libc
 #
 echo Bootstrapping libc
 cd $S/lib/libc
 make clean
-make
+make all
 make install
 make clean
 
@@ -32,7 +63,56 @@ rm -f ranlib
 ranlib /lib/libc.a
 
 #
-# Well, let's go for broke, eh?
+# Now we need to install a few binaries needed for the rest of the build
 #
-cd $s
-make build
+echo Bootrapping ar and strip
+cd $S/bin
+make ar strip
+cp ar strip /bin
+rm ar strip
+
+#
+# Now we need to bootstrap the kernel config. The undo process leads us to a
+# place where /sys is inconsistent. This restores consistency.  Or at least
+# generates a better sys/h/localdefs.h for building everything else.
+#
+cd $S/sys/conf
+./config GENERIC
+
+#
+# Build and install the C compiler. Rebuild and install
+# the C library. Rebuild and reinstall the C compiler.
+# (likely overkill).
+#
+# Then build and install all the libraries in $S/usr.lib,
+# but not the programs, they need these same libraries
+# so we'll catch them later.
+#
+# Finally, do a simple make all.
+#
+# Make build is almost the same as this (gets details
+# wrong about usr.lib), but fails in building libc the
+# second time for reasons unknown. This sequence should
+# be completely reproducible.
+#
+cd $S
+make clean
+cd lib
+for i in ccom cpp c2 libc ccom cpp c2; do
+    (cd $i; make all; make install; make clean)
+done
+cd ../usr.lib
+for i in lib*; do
+    (cd $i; make all; make install; make clean)
+done
+cd ..
+make all
+make install
+make clean
+
+#
+# Now build and install the kernel. Oh, wait that's broken still.
+# So we won't do that, won't try to build the stand programs nor
+# create a tape image (or at least set of TAPE files). That's
+# later on the hit parade.
+#
